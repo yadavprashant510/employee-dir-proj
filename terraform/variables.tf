10189465d535571d9f4893ffc4141fa782e2cba7
Prashant Yadav <yadavprashant510@gmail.com>
2026-06-09 18:36:19 +0530

    Added the terraform file

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "Select the provided region in which you want to install the application"
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "Select the provided instance type in which you want to install the application"
+  default = "t2.micro"
+}
+
